71c99b13268f1ac62e4c71ab8af2b603afb95b88
jhonlewis464 <136278129+jhonlewis464@users.noreply.github.com>
2023-08-23 15:22:26 +0500

    Counter.js with css

--- Counter.js
+++ Counter.js
@@ -1,7 +1,29 @@
 // Javascript Counter
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<style>
+		body {
+			text-align: center;
+			font-family: Arial, sans-serif;
+		}
 
+		h1 {
+			color: #007bff;
+		}
+
+		#counter {
+			font-size: 48px;
+			color: #28a745;
+			margin-top: 20px;
+		}
+
+		p {
+			font-size: 18px;
+			color: #333;
+		}
+	</style>
+</head>
 <body style="text-align:center">
 	<h1>GeeksforGeeks</h1>
 	<p>COUNTS</p>
